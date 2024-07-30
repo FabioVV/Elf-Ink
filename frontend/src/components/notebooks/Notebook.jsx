f18f6e2ea1e6513bfb33935ffd94660255f79927
FabioVV <fabio-varela@protonmail.com>
2024-07-30 10:50:57 -0300

    fix some styles on notebook

--- frontend/src/components/notebooks/Notebook.jsx
+++ frontend/src/components/notebooks/Notebook.jsx
@@ -3,17 +3,25 @@ import {useState} from "react"
 function Notebook({notebook, handleFetch, handleActiveNotebook}) {
 
   const [display, setDisplay] = useState(false)
+  const [classFont, setClassFont] = useState('fa-solid fa-chevron-right')
 
   function openNotebookOptions(){
     setDisplay(prevDisplay => !prevDisplay);
+    if(classFont == 'fa-solid fa-chevron-right'){
+      setClassFont('fa-solid fa-angle-down')
+    } else {
+      setClassFont('fa-solid fa-chevron-right')
+    }
   }
 
   return (
     <>
       <li title={notebook?.title}>
-        <span onClick={openNotebookOptions} className='list-options'><i className="fa-solid fa-list-ul"></i></span>
+        <span onClick={openNotebookOptions} className='list-options'>
+          <i className={classFont}></i>
+        </span>
         <span onClick={()=>handleActiveNotebook(notebook)} className={notebook?.active == false ? "list-item": "list-item active-notebook"}>{notebook?.title} </span>
-        <span className="">{notebook?.leaf_count}</span> 
+        <span className="stick-right">{notebook?.leaf_count}</span> 
       </li>
 
       <span className={`list-subitem ${display ? 'show' : ''}`}>
