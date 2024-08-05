40f5125d3b65fe528ae544dc9df73d7dbeac464f
FabioVV <fabio-varela@protonmail.com>
2024-08-05 15:40:33 -0300

    add last partial to notebook deletion. TODO: create function to actually delete notebook

--- frontend/src/components/notebooks/Notebook.jsx
+++ frontend/src/components/notebooks/Notebook.jsx
@@ -25,24 +25,38 @@ function Notebook({notebook, handleFetch, handleActiveNotebook, activeNotebook})
 
     notebook_title_field.innerHTML = `
       <input autofocus id='new_notebook_title' type='text' value='${notebook?.title}'>
+      <br>
+      <i id="delete_notebook" 
+      title='Delete notebook' 
+      style='color:red; font-size:18px; z-index:100;' 
+      class="fa-solid fa-trash-can"
+      align="center"
+      ></i>
     `
+
+    document.getElementById('delete_notebook').addEventListener('click', (e) => {
+      alert('delete notebook')
+      document.getElementById(`${notebook?.ID}`).remove()
+    })
+
     notebook_title_field.addEventListener('keydown', (e) => {
       if (e.key === 'Enter') {
+        alert('submit new notebook name')
 
         let val = document.getElementById('new_notebook_title').value
 
         notebook_title_field.innerHTML = `
           ${val}
         `
-
       }
     })
 
   }
 
+
   return (
     <>
-      <li title={notebook?.title}>
+      <li id={notebook?.ID} title={notebook?.title}>
         <span onClick={openNotebookOptions} className='list-options'>
           <i className={classFont}></i>
         </span>
